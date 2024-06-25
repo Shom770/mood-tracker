e6d549ab4eaf6e3aaefc2e6c0ea0468deda09d75
Shom770 <shayaanwadkar@gmail.com>
2024-06-25 17:26:14 -0400

    design finished

--- MoodTracker/App.js
+++ MoodTracker/App.js
@@ -1,5 +1,5 @@
 import "./global.css"
-import { Text, View } from 'react-native';
+import { TouchableOpacity, Text, TextInput, View } from 'react-native';
 import { Icon } from '@rneui/themed';
 import MoodSelector from './components/MoodSelector.js'
 
@@ -17,14 +17,25 @@ export default function App() {
           How was your day today?
         </Text>
         <View className="flex flex-row gap-2 items-start justify-center">
-            <MoodSelector color="bg-red-400" text="1" />
-            <MoodSelector color="bg-red-200" text="2" />
-            <MoodSelector color="bg-gray-200" text="3" />
-            <MoodSelector color="bg-green-200" text="4" />
-            <MoodSelector color="bg-green-400" text="5" />
+            <MoodSelector color="bg-red-400" text="1" iconName="sad-cry" iconType="font-awesome-5" size={33}/>
+            <MoodSelector color="bg-red-200" text="2" iconName="emoticon-sad-outline" iconType="material-community"/>
+            <MoodSelector color="bg-gray-200" text="3" iconName="emoticon-neutral-outline" iconType="material-community"/>
+            <MoodSelector color="bg-green-200" text="4" iconName="smile" iconType="feather"/>
+            <MoodSelector color="bg-green-400" text="5" iconName="smile-o" iconType="font-awesome"/>
         </View>
+      </View>
+      <View className="flex flex-col gap-1 items-start justify-center ml-[5vw]">
+        <Text className="text-white text-xl font-medium mt-5">
+          Anything noteworthy about today?
+        </Text>
+        <TextInput className="bg-gray-400/[0.1] w-[90vw] h-[20vh] rounded-xl border-2 border-gray-400 px-4 py-4 text-white" placeholder="Enter your thoughts here." placeholderTextColor="#909090" multiline={true} blurOnSubmit={true}>
+        </TextInput>
+      </View>
+      <View className="flex items-center justify-center mt-8 w-full">
+        <TouchableOpacity className="flex items-center justify-center bg-amber-300 rounded-2xl w-[90%] h-20">
+          <Text className="font-extrabold text-black text-2xl">Submit</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
-
--- MoodTracker/components/MoodSelector.js
+++ MoodTracker/components/MoodSelector.js
@@ -1,12 +1,14 @@
 import "../global.css"
 import React from "react";
-import { TouchableOpacity, View, Text } from 'react-native';
+import { TouchableOpacity, View, Text, TouchableHighlight } from 'react-native';
+import { Icon } from '@rneui/themed';
 
 export default function MoodSelector(props) {
     return (
         <TouchableOpacity onPress={() => console.log('pressed')}>
-            <View className={`flex w-16 h-16 items-center justify-center rounded-lg ${props.color}`}>
-                <Text className="text-[#0d1117] text-3xl font-semibold">
+            <View className={`flex flex-col w-20 h-20 items-center justify-center rounded-lg ${props.color}`}>
+                <Icon name={props.iconName} type={props.iconType} size={!props.size ? 36 : props.size} color="#0D1117"/>
+                <Text className="text-[#0d1117] text-xl font-light">
                     {props.text}
                 </Text>
             </View>
